d2cc1fc8c06b3ddb9007b316cbcb2292bab583cd
Shobhit Bhatt <shobhitbhatt.sb@gmail.com>
2022-07-27 12:33:44 +0530

    commit

--- Barclays_Payments_Project/src/test/java/com/barclays/BarclaysPaymentsProjectTests.java
+++ Barclays_Payments_Project/src/test/java/com/barclays/BarclaysPaymentsProjectTests.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 
 import com.barclays.dto.UserDTO;
+import com.barclays.entity.Bills;
 import com.barclays.entity.User;
 import com.barclays.exception.PaymentsException;
 import com.barclays.repository.UserRespository;
@@ -29,24 +30,19 @@ class BarclaysPaymentsProjectTests {
 	
 	@Test
 	public void authenticateUserTestValidCredentials() throws PaymentsException {
-		UserDTO user = new UserDTO();
-		user.setLoginId(123456);
-		user.setPassword("123");
-		Mockito.when(userRepository.findById(123456));
-		ResponseEntity<String> actual = userService.loginUser(user);
-		Assertions.assertEquals("SUCCESS", actual);
+		
+		UserDTO User = new UserDTO();
+		User.setLoginId(1000001);
+		User.setPassword("123");
+		ResponseEntity<String> actual = userService.loginUser(User);
+		Assertions.assertEquals("User successfully logged in :1000001ASAccount_Holder", actual);
 	} 
 	@Test
 	public void authenticateUserTestInValidCredentials() throws PaymentsException {
 		UserDTO User = new UserDTO();
-		User.setLoginId(123456);
-		User.setPassword("monica12");
+		User.setLoginId(1000001);
+		User.setPassword("1");
 		
-		UserDTO fromRepository = new UserDTO();
-		fromRepository.setLoginId(123456);
-		fromRepository.setPassword("123");
-		
-		Mockito.doReturn(fromRepository).when(userRepository.findById(123456));
 	    PaymentsException exception=Assertions.assertThrows(PaymentsException.class,()->userService.loginUser(User));
 		Assertions.assertEquals("Service.INCORRECT", exception.getMessage());
 	}
